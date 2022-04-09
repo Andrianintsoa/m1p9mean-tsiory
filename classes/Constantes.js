63e3ff8101eed490fe1549f271180d55ffb66140
andrianintsoa <andrianintsoat@gmail.com>
2022-04-09 10:52:30 +0300

    [Express] Constantes Type utilisateur

---
+++ classes/Constantes.js
@@ -0,0 +1,8 @@
+let Constantes = class {
+  static typeClient() { return "625139ddad79350000624d4d" }
+  static typeAdmin() { return "62513a1fad79350000624d50" }
+  static typeResto() { return "62513a27ad79350000624d51" }
+  static typeLivreur() { return "62513a2dad79350000624d52" }
+  
+}
+module.exports = Constantes;
